89df560c3eeca8f347076671c48ac5de7cf18450
ShaunTheSleepingSheep <>
2021-07-25 18:29:45 +0800

    Something

--- C206_ResourceCentre/src/c206_Part1/HelloWorld.java
+++ C206_ResourceCentre/src/c206_Part1/HelloWorld.java
@@ -1,10 +1,3 @@
-/**
- * I declare that this code was written by me.
- * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism
- *
- * limsean, Jul 25, 2021 5:43:04 PM
- */
 package c206_Part1;
 
 public class HelloWorld {
