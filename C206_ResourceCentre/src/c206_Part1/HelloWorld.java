7f7a3345a86e35d14392432a32d6e0745907a7f6
Shaun <ShaunTheSleepingSheep>
2021-07-25 17:53:50 +0800

    Created HelloWorld.java

---
+++ C206_ResourceCentre/src/c206_Part1/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism
+ *
+ * limsean, Jul 25, 2021 5:43:04 PM
+ */
+package c206_Part1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
